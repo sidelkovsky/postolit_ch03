925900fafd272b5c4928ed7335d6a890d6aba79e
sidelkovsky <sidelkovsky@tuta.io>
2023-07-19 21:23:04 +0400

    more routes and views

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -12,3 +12,13 @@ def about(request):
 
 def contact(request):
     return HttpResponse("<h2>Контакты</h2>")
+
+
+def products(request, productid = 1):
+    output = "<h2>Product # {}</h2".format(productid)
+    return HttpResponse(output)
+
+
+def users(request, id=1, name="Valera"):
+    output = "<h2>User</h2><h3>id: {0}<br/>name: {1}</h3>".format(id, name)
+    return HttpResponse(output)
--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -23,4 +23,11 @@ urlpatterns = [
     path('', views.index, name='home'),
     re_path(r'^about', views.about),
     re_path(r'^contact', views.contact),
+    # re_path(r'^products/$', views.products),
+    # re_path(r'^products/(?P<productid>\d+)/', views.products),
+    # re_path(r'^users/(?P<id>\d+)/(?P<name>\D+)/', views.users),
+    path('products/', views.products),
+    path('products/<int:productid>/', views.products),
+    path('users/', views.users),
+    path('users/<int:id>/<str:name>/', views.users),
 ]
